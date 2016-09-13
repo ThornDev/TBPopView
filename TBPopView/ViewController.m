5fc3f1b4cdcfbf575eaa18e9290d48aef43bfb9b
algolstep <jiemianwang@gmail.com>
2016-09-13 13:34:05 +0800

    add two buttons

--- TBPopView/ViewController.m
+++ TBPopView/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController ()
 {
     TBPopoverView *pop;
+    TBPopoverView *pop2;
+    TBPopoverView *pop3;
 }
 @end
 
@@ -28,11 +30,11 @@
 - (IBAction)showPopAction:(UIButton*)sender {
     NSArray *titles = @[@"未评", @"初级", @"中级",@"高级"];
     NSArray *images = @[@"1", @"2", @"3",@"4"];
-    if (!pop) {
+    if ((!pop) && sender.tag == 1) {
         pop = [[TBPopoverView alloc] initWithTouchView:sender popWidth:200];
         [pop setTitleArray:titles];
         [pop setImageArray:images];
-        [pop setArrowPosition:ArrowPositionInRight];
+        
         [pop setFontSize:18.0f];
         [pop setFontColor:[UIColor whiteColor]];
         
@@ -41,7 +43,51 @@
         };
     }
     
-    [pop show];
+    if ((!pop2) && sender.tag == 2) {
+        pop2 = [[TBPopoverView alloc] initWithTouchView:sender popWidth:200];
+        [pop2 setTitleArray:titles];
+        [pop2 setImageArray:images];
+        
+        [pop2 setFontSize:18.0f];
+        [pop2 setFontColor:[UIColor whiteColor]];
+        
+        pop2.selectRowAtIndex = ^(NSInteger index){
+            [sender setTitle:titles[index] forState:UIControlStateNormal];
+        };
+    }
+    
+    if ((!pop3) && sender.tag == 3) {
+        pop3 = [[TBPopoverView alloc] initWithTouchView:sender popWidth:200];
+        [pop3 setTitleArray:titles];
+        [pop3 setImageArray:images];
+        
+        [pop3 setFontSize:18.0f];
+        [pop3 setFontColor:[UIColor whiteColor]];
+        
+        pop3.selectRowAtIndex = ^(NSInteger index){
+            [sender setTitle:titles[index] forState:UIControlStateNormal];
+        };
+    }
+    
+    
+    switch (sender.tag) {
+        case 1:
+            [pop setArrowPosition:ArrowPositionInLeft];
+            [pop show];
+            break;
+        case 2:
+            [pop2 setArrowPosition:ArrowPositinInMiddle];
+            [pop2 show];
+            break;
+        case 3:
+            [pop3 setArrowPosition:ArrowPositionInRight];
+            [pop3 show];
+            break;
+            
+        default:
+            break;
+    }
+    
 }
 
 @end
